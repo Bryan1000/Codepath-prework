08d6d8fca9db998ac3d2dde3b2e02d563ce51c52
Bryan1000 <60412740+Bryan1000@users.noreply.github.com>
2022-01-21 18:20:15 -0800

    Updated readme

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -14,6 +14,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+   
+    @IBOutlet weak var TextFieldReset: UITextField!
+    @IBOutlet var ViewColorReset: UIView!
+    @IBOutlet weak var TextLabelReset: UILabel!
+    @IBAction func TapReset(_ sender: Any) {
+        TextLabelReset.textColor = UIColor.red
+        TextLabelReset.text = "Hello from Bryan!"
+        ViewColorReset.backgroundColor = UIColor.systemTeal
+        TextFieldReset.text = ""
+    }
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var TextLabel2: UILabel!
+    @IBAction func ChangeText(_ sender: Any) {
+        var result : String?
+        result = TextField.text
+        TextLabel2.text = result
+        
+        if result == ""
+        {
+            TextLabel2.text = "Goodbye 👋"
+        }
+        self.view.endEditing(true)
+        TextField.text = ""
+    }
+        
+    
+    
+    @IBOutlet var ViewColor: UIView!
+    @IBAction func ButtonClicked2(_ sender: Any) {
+        print("hello bud")
+        ViewColor.backgroundColor = UIColor.green
+    }
     @IBOutlet weak var TextLabel: UILabel!
     @IBAction func ButtonClicked(_ sender: Any) {
         print("hello")
